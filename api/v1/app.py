78fe97cb2e0b24aa00d8cb2b27ee6752cd573671
swiftleah <leahrosehenry@gmail.com>
2024-01-06 11:33:24 +0000

    Installed pycodestyle version 2.0.0 to work with pythoin 3.4.3 - fixed pycodestyle error in api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ registers blueprint """
+import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 app = Flask(__name__)
+
 
 @app.teardown_appcontext
 def close_storage(exception):
