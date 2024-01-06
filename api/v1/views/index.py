677fda4350df9d14ff196ffd62016d1a7c8264e7
swiftleah <leahrosehenry@gmail.com>
2024-01-06 11:51:16 +0000

    Fixed more pycodestyle errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,11 @@
 from flask import jsonify
 from . import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/api/v1/stats', methods=['GET'])
 def get_stats():
